a5cbcaefb09d0b19898f5cbd1f1cac8132d20abe
Emilio Wuerges <wuerges@gmail.com>
2016-01-21 16:13:58 -0200

    fixed indentation for gui.py

--- gui.py
+++ gui.py
@@ -7,36 +7,36 @@ import time
 
 
 class TestStore(Gtk.ListStore):
-  def __init__(self):
-    Gtk.ListStore.__init__(self, str, int, bool)
-    self.tests = []
+    def __init__(self):
+        Gtk.ListStore.__init__(self, str, int, bool)
+        self.tests = []
   
-  def append(self, v):
-    t = tester.Test(v[0])
-    self.tests.append(t)
-    return Gtk.ListStore.append(self, v)
+    def append(self, v):
+        t = tester.Test(v[0])
+        self.tests.append(t)
+        return Gtk.ListStore.append(self, v)
 
-  def modify(self, path, text):
-    self[int(path)][0] = text
-    self.tests[int(path)].url = text
+    def modify(self, path, text):
+        self[int(path)][0] = text
+        self.tests[int(path)].url = text
 
-  def reset(self):
-    self.tests = []
-    self.clear()
-    for i in range(10):
-      #self.liststore.append(["http://mirror.internode.on.net/pub/test/1meg.test", 0.0, False])
-      self.append(["                                                                          ", 0.0, False])
+    def reset(self):
+        self.tests = []
+        self.clear()
+        for i in range(10):
+            #self.liststore.append(["http://mirror.internode.on.net/pub/test/1meg.test", 0.0, False])
+            self.append(["                                                                          ", 0.0, False])
 
-  def save_state(self, fn):
-    with open(fn, 'wb') as f:
-      pickle.dump((self.tests, list(self)), f)
+    def save_state(self, fn):
+        with open(fn, 'wb') as f:
+            pickle.dump((self.tests, list(self)), f)
 
-  def load_state(self, fn):
-    with open(fn, 'rb') as f:
-      (self.tests, lself) = pickle.load(self.tests)
-      self.clear()
-      for r in lself:
-        Gtk.ListStore.append(self, r)
+    def load_state(self, fn):
+        with open(fn, 'rb') as f:
+            (self.tests, lself) = pickle.load(self.tests)
+            self.clear()
+            for r in lself:
+                Gtk.ListStore.append(self, r)
 
 class CellRendererProgressWindow(Gtk.Window):
 
@@ -93,96 +93,96 @@ class CellRendererProgressWindow(Gtk.Window):
         GObject.timeout_add(3600 * 1000, self.hourly_timeout, None)
 
     def show_results_now(self, button):
-      dialog = Gtk.FileChooserDialog("Please choose a file", self,
-        Gtk.FileChooserAction.SAVE,
-        (Gtk.STOCK_CANCEL, Gtk.ResponseType.CANCEL,
-        Gtk.STOCK_SAVE, Gtk.ResponseType.OK))
-      dialog.set_current_name("Untitled.csv")
+        dialog = Gtk.FileChooserDialog("Please choose a file", self,
+            Gtk.FileChooserAction.SAVE,
+            (Gtk.STOCK_CANCEL, Gtk.ResponseType.CANCEL,
+            Gtk.STOCK_SAVE, Gtk.ResponseType.OK))
+        dialog.set_current_name("Untitled.csv")
 
-      response = dialog.run()
-      if response == Gtk.ResponseType.OK:
-        print("Open clicked")
-        print("File selected: " + dialog.get_filename())
-      elif response == Gtk.ResponseType.CANCEL:
-        print("Cancel clicked")
+        response = dialog.run()
+        if response == Gtk.ResponseType.OK:
+            print("Open clicked")
+            print("File selected: " + dialog.get_filename())
+        elif response == Gtk.ResponseType.CANCEL:
+            print("Cancel clicked")
 
-      dialog.destroy()
+        dialog.destroy()
 
     def reset_now(self, button):
-      self.liststore.reset()
-      if self.test_thread:
-        self.test_thread.join(0.0)
-      self.update_progress_bars()
+        self.liststore.reset()
+        if self.test_thread:
+          self.test_thread.join(0.0)
+        self.update_progress_bars()
 
     def text_edited(self, widget, path, text):
-      self.liststore.modify(path, text)
+        self.liststore.modify(path, text)
 
     def on_inverted_toggled(self, button, path):
-      self.liststore[path][2] = not self.liststore[path][2]
+        self.liststore[path][2] = not self.liststore[path][2]
 
     def start_new_test_thread(self):
-      """
-      performs the tests
-      and setups the update bars timeout
-      """
-      GObject.timeout_add(100, self.on_timeout, None)
-      def run_tests():
-        with self.lock:
-          for l, t in zip(self.liststore, self.liststore.tests):
-            if l[2]:
-              t.test()
-          for l, t in zip(self.liststore, self.liststore.tests):
-            if l[2]:
-              print(t.results)
-
-      if not self.test_thread:
-        self.test_thread = threading.Thread(target=run_tests)
-        self.test_thread.start()
+        """
+        performs the tests
+        and setups the update bars timeout
+        """
+        GObject.timeout_add(100, self.on_timeout, None)
+        def run_tests():
+            with self.lock:
+                for l, t in zip(self.liststore, self.liststore.tests):
+                    if l[2]:
+                        t.test()
+                for l, t in zip(self.liststore, self.liststore.tests):
+                    if l[2]:
+                        print(t.results)
+               
+        if not self.test_thread:
+            self.test_thread = threading.Thread(target=run_tests)
+            self.test_thread.start()
 
     def test_now(self, button):
-      self.testB.set_sensitive(False)
-      self.start_new_test_thread()
-      return True
+        self.testB.set_sensitive(False)
+        self.start_new_test_thread()
+        return True
 
     def update_progress_bars(self):
-      for l, t in zip(self.liststore, self.liststore.tests):
-        l[1] = t.progress
+        for l, t in zip(self.liststore, self.liststore.tests):
+            l[1] = t.progress
 
     def hourly_timeout(self, user_data):
-      self.start_new_test_thread()
+        self.start_new_test_thread()
 
     def write_csv(self, filename):
-      with open(filename, 'w') as f:
-        w = csv.writer(f)
-        for t in self.liststore.tests:
-          w.writerows(t.result_lines())
+        with open(filename, 'w') as f:
+            w = csv.writer(f)
+            for t in self.liststore.tests:
+                w.writerows(t.result_lines())
 
     def save_state(self, filename):
-      self.liststore.save_state(filename)
+        self.liststore.save_state(filename)
 
     def load_state(self, filename):
-      self.liststore.load_state(filename)
+        self.liststore.load_state(filename)
 
     def on_timeout(self, user_data):
-      """ 
-      Updates progress bars and makes the button
-      sensitive once there is not a thread running anymore
-      """
-      self.update_progress_bars()
-
-      if self.test_thread: 
-        if self.test_thread.is_alive():
-          self.testB.set_sensitive(False)
+        """ 
+        Updates progress bars and makes the button
+        sensitive once there is not a thread running anymore
+        """
+        self.update_progress_bars()
+       
+        if self.test_thread: 
+            if self.test_thread.is_alive():
+                self.testB.set_sensitive(False)
+            else:
+                self.test_thread.join()
+                self.test_thread = None
+                self.testB.set_sensitive(True)
+                return False
         else:
-          self.test_thread.join()
-          self.test_thread = None
-          self.testB.set_sensitive(True)
-          return False
-      else:
-        self.testB.set_sensitive(True)
-        return False
-
-      return True
+            self.testB.set_sensitive(True)
+            return False
+       
+        return True
 
 GObject.threads_init()
 win = CellRendererProgressWindow()
@@ -190,5 +190,5 @@ win.connect("delete-event", Gtk.main_quit)
 win.show_all()
 Gtk.main()
 if win.test_thread:
-  win.test_thread.join(0.0)
+    win.test_thread.join(0.0)
 
