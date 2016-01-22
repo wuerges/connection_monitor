768ba97e1fe0e3cbf12532f1c545af630367384c
Emilio Wuerges <wuerges@gmail.com>
2016-01-22 12:37:45 -0200

    changing setup.py

--- setup.py
+++ setup.py
@@ -90,14 +90,14 @@ setup(
     # installed, specify them here.  If using Python 2.6 or less, then these
     # have to be included in MANIFEST.in as well.
     #package_data={
-    #    'icons': ['imgs/icon.png'],
+    #    'pycomon': ['imgs/icon.png'],
     #},
 
     # Although 'package_data' is the preferred approach, in some case you may
     # need to place data files outside of your packages. See:
     # http://docs.python.org/3.4/distutils/setupscript.html#installing-additional-files # noqa
     # In this case, 'data_file' will be installed into '<sys.prefix>/my_data'
-    #data_files=[('imgs', ['imgs/icon.png'])],
+    data_files=[('imgs', ['imgs/icon.png'])],
 
     # To provide executable scripts, use entry points in preference to the
     # "scripts" keyword. Entry points provide cross-platform support and allow
